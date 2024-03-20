7eab354485f6692bc1e43d5226a7e5a62287d96b
HishambassamB <hishembassam@gmail.com>
2024-03-21 00:45:59 +0300

    Pulse Check

---
+++ main.js
@@ -0,0 +1,59 @@
+/* START CODE UNDER THIS LINE */
+////Pulse Check///
+const person_1 ={
+    name:"hisham",
+    age:21,
+    gender:"male",
+}
+
+
+
+const movie ={
+    name:'john wick',
+    length:'1:55',
+    gender:'actio',
+
+}
+
+const favoriteFood ={
+    mhyfavoriteFood :['mansaf','pizza','burger']
+
+}
+const store ={
+    name:'kahramanshose',
+    address: {buildingNumber:14, street:'Madaba Tourism Street'}  
+   }
+
+
+
+
+//q2///
+// a- using a dot notation
+const orderedSolarSystem = {
+  Mercery: "first",
+  Venus: "second",
+  Earth: "third",
+  Mars: "fourth",
+
+};
+orderedSolarSystem.Mars
+console.log(orderedSolarSystem.Mars);
+// b- using a bracket notation
+const unorderedSolarSystem = {
+  Mars: "fourth",
+  Earth: "third",
+  Mercery: "first",
+  Venus: "second",
+};
+unorderedSolarSystem["Mars"]
+
+////q3////
+//Add the math grade (80) to the student using a dot notation
+///Change the English grade to a 90 using a dot notation.
+const studentOne = {
+  englishGrade: 80,
+  scienceGrade: 90,
+  studentOne.push("englishGrade":90)
+};
+
+
